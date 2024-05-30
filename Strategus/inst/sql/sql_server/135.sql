5ce3b7b392c7c94daeb3b5d5280be1640a9b3a57
Milou Brand <milou.brand@iqvia.com>
2024-05-30 10:36:44 +0000

    removing some echocardiogram codes

--- Strategus/inst/sql/sql_server/135.sql
+++ Strategus/inst/sql/sql_server/135.sql
@@ -48,6 +48,17 @@ UNION  select c.concept_id
   and c.invalid_reason is null
 
 ) I
+LEFT JOIN
+(
+  select concept_id from @vocabulary_database_schema.CONCEPT where concept_id in (759555,45773079)
+UNION  select c.concept_id
+  from @vocabulary_database_schema.CONCEPT c
+  join @vocabulary_database_schema.CONCEPT_ANCESTOR ca on c.concept_id = ca.descendant_concept_id
+  and ca.ancestor_concept_id in (759555,45773079)
+  and c.invalid_reason is null
+
+) E ON I.concept_id = E.concept_id
+WHERE E.concept_id is null
 ) C UNION ALL 
 SELECT 49 as codeset_id, c.concept_id FROM (select distinct I.concept_id FROM
 ( 
